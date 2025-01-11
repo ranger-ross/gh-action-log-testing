2cc16795ed1af424c214a12bf5f365f41a70d5e5
Ross Sullivan <rosssullivan101@gmail.com>
2025-01-11 13:51:36 +0900

    just pushing it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::io::{self, Write};
+use std::io::{self, IsTerminal, Write};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() {
+    println!("stdout = {}", std::io::stdout().is_terminal());
+    println!("stderr = {}", std::io::stderr().is_terminal());
+
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout
         .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
